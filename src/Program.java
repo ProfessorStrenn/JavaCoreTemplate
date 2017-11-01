e1eec3af07fcd6fcd3c9e32a8f463336856d3511
Stephen Strenn <spstrenn@pipeline.sbcc.edu>
2017-11-01 09:36:08 -0700

    initial commit

---
+++ src/Program.java
@@ -0,0 +1,17 @@
+import static sbcc.Core.*;
+import static java.lang.System.*;
+import static org.apache.commons.lang3.StringUtils.*;
+
+/**
+ * This class contains main(), the starting point for the application.
+ * 
+ * @author YOUR_NAME_HERE
+ *
+ */
+public class Program {
+
+	public static void main(String[] args) {
+
+	}
+
+}
