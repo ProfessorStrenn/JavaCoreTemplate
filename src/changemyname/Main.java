03e409b8fb5d2b015112110729e83aa61e9a5932
ProfessorStrenn <ProfessorStrenn@users.noreply.github.com>
2022-10-03 22:27:27 -0700

    Initial conversion to Intellij

--- src/changemyname/Main.java
+++ src/changemyname/Main.java
@@ -1,18 +1,17 @@
 package changemyname;
 
+import java.io.IOException;
+import java.util.Map;
+
 import static sbcc.Core.*;
 import static java.lang.System.*;
 import static org.apache.commons.lang3.StringUtils.*;
+import static java.util.Arrays.*;
+import static java.lang.Math.*;
 
-/**
- * 
- * @author your_name_here
- *
- */
 public class Main {
 
-	public static void main(String[] args) {
-
-	}
+    public static void main(String[] args) throws IOException {
+    }
 
 }
