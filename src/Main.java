1c46b48aae68ea6a662f3237a5afbf8a1f74fa06
ProfessorStrenn <ProfessorStrenn@users.noreply.github.com>
2022-10-07 13:30:45 -0700

    refactored Main into default package

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
-package edu.sbcc.cs;
-
 import static sbcc.Core.*;
 import static java.lang.System.*;
 import static org.apache.commons.lang3.StringUtils.*;
 import static java.util.Arrays.*;
 import static java.lang.Math.*;
+
 
 public class Main {
 
