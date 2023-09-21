b4fd7896a3bef566c2d818816f5a72b431a1da7b
ProfessorStrenn <ProfessorStrenn@users.noreply.github.com>
2023-09-20 20:42:58 -0700

    Removed import java.util.Arrays.*

--- src/cs106/Main.java
+++ src/cs106/Main.java
@@ -3,13 +3,12 @@ package cs106;
 import static sbcc.Core.*;
 import static java.lang.System.*;
 import static org.apache.commons.lang3.StringUtils.*;
-import static java.util.Arrays.*;
 import static java.lang.Math.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        
+
     }
 
 }
